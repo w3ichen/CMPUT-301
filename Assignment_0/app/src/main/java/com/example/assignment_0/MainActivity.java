9b230133bc4856fe10a76f780712df6294e79960
weichen <w3ichen@gmail.com>
2022-01-11 11:47:24 -0700

    Assignment 0 done

---
+++ Assignment_0/app/src/main/java/com/example/assignment_0/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.assignment_0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.application_0.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)  findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
