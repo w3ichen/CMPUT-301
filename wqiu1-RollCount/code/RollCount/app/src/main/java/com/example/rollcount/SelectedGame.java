ad01ae5be1f0397605ab3cb2bba4ca4b5ed21c88
weichen <w3ichen@gmail.com>
2022-01-29 17:08:38 -0700

    Name change

--- wqiu1-RollCount/code/RollCount/app/src/main/java/com/example/rollcount/SelectedGame.java
+++ wqiu1-RollCount/code/RollCount/app/src/main/java/com/example/rollcount/SelectedGame.java
@@ -25,14 +25,18 @@ public class SelectedGame extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_play, R.id.navigation_stats, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_selected_game);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+   
+
+
 
         // Get Game content
         Intent intent = getIntent();
