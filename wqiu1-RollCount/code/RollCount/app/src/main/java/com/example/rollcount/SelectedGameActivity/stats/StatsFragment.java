33dd77723572d47a1f7ead8a278b9d09685ab791
weichen <w3ichen@gmail.com>
2022-01-30 08:39:20 -0700

    Save work from yesterday

--- wqiu1-RollCount/code/RollCount/app/src/main/java/com/example/rollcount/SelectedGameActivity/stats/StatsFragment.java
+++ wqiu1-RollCount/code/RollCount/app/src/main/java/com/example/rollcount/SelectedGameActivity/stats/StatsFragment.java
@@ -1,33 +1,38 @@
 package com.example.rollcount.SelectedGameActivity.stats;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.example.rollcount.Game;
+import com.example.rollcount.MainActivity;
 import com.example.rollcount.R;
 import com.example.rollcount.databinding.StatsTabBinding;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class StatsFragment extends Fragment {
 
     private StatsTabBinding binding;
 
     // Credits: https://medium.com/@mobindustry/how-to-quickly-implement-beautiful-charts-in-your-android-app-cf4caf050772
+    // Credits: https://www.geeksforgeeks.org/how-to-create-a-barchart-in-android/
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -35,64 +40,41 @@ public class StatsFragment extends Fragment {
         View root = binding.getRoot();
 
         BarChart chart = (HorizontalBarChart) root.findViewById(R.id.chart);
-        BarData data = new BarData( (IBarDataSet) getXAxisValues(),(IBarDataSet) getDataSet());
-        chart.setData(data);
+        BarDataSet data = new BarDataSet(getDataSet(), "counts");
+        data.setValueTextSize(20f);
+        data.setValueTextColor(Color.BLACK);
+        data.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        chart.setData(new BarData(data));
         chart.animateXY(2000, 2000);
         chart.invalidate();
 
+        // Format the x axis to strings
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return "Roll " + Math.round(value);
+
+            }
+        });
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); //  move to left side
+        xAxis.setTextSize(20f); // increase font size
+
         return root;
     }
-    private ArrayList getDataSet() {
-        ArrayList dataSets = null;
 
-        ArrayList valueSet1 = new ArrayList();
-        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
-        valueSet1.add(v1e1);
-        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
-        valueSet1.add(v1e2);
-        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
-        valueSet1.add(v1e3);
-        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
-        valueSet1.add(v1e4);
-        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
-        valueSet1.add(v1e5);
-        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
-        valueSet1.add(v1e6);
+    private ArrayList<BarEntry> getDataSet() {
+        Intent intent = getActivity().getIntent();
+        Game game = intent.getParcelableExtra(MainActivity.SELECTED_GAME);
+        ArrayList<Integer> rollCounts = (ArrayList<Integer>) intent.getSerializableExtra(MainActivity.SELECTED_GAME_ROLL_COUNTS);
+        ArrayList<BarEntry> barEntries = new ArrayList<BarEntry>();
 
-        ArrayList valueSet2 = new ArrayList();
-        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
-        valueSet2.add(v2e1);
-        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
-        valueSet2.add(v2e2);
-        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
-        valueSet2.add(v2e3);
-        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
-        valueSet2.add(v2e4);
-        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
-        valueSet2.add(v2e5);
-        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
-        valueSet2.add(v2e6);
+        for (int i = 0; i < rollCounts.size(); i++) {
+            barEntries.add(new BarEntry(game.getRollNumFromPosition(i), rollCounts.get(i)));
+        }
 
-        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
-        barDataSet1.setColor(Color.rgb(0, 155, 0));
-        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
-        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
-
-        dataSets = new ArrayList();
-        dataSets.add(barDataSet1);
-        dataSets.add(barDataSet2);
-        return dataSets;
-    }
-
-    private ArrayList getXAxisValues() {
-        ArrayList xAxis = new ArrayList();
-        xAxis.add("JAN");
-        xAxis.add("FEB");
-        xAxis.add("MAR");
-        xAxis.add("APR");
-        xAxis.add("MAY");
-        xAxis.add("JUN");
-        return xAxis;
+        return barEntries;
     }
 
     @Override
