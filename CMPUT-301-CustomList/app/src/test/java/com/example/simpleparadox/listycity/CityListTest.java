16df6f1fdb2b75e13ee02dd08808f616cda0fdf4
weichen <w3ichen@gmail.com>
2022-02-16 15:14:50 -0700

    JUnit Lab is done

--- CMPUT-301-CustomList/app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ CMPUT-301-CustomList/app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -20,5 +20,35 @@ public class CityListTest {
         CityList cityList = mockCityList();
 
         assertEquals(1, cityList.getCities().size());
+
+        City city = new City("Regina","Saskatchewan");
+        cityList.add(city);
+
+        assertEquals(2, cityList.getCities().size());
+        assertTrue(cityList.getCities().contains((city)));
+    }
+
+    @Test
+    void testAddException(){
+        CityList cityList = mockCityList();
+        City city = new City("Yellowknife","NWT");
+        cityList.add(city);
+
+        assertThrows(IllegalArgumentException.class, ()->{
+            cityList.add(city);
+        });
+    }
+
+    @Test
+    void testGet(){
+        CityList cityList = mockCityList();
+
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
+
+        City city = new City("Charlottetown","PEI");
+        cityList.add(city);
+
+        assertEquals(0, city.compareTo(cityList.getCities().get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));
     }
 }
